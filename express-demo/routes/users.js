782d142ba17485ecb0e5187a8a4cecab955be18e
dianligegege <1220671124@qq.com>
2021-06-03 00:01:53 +0800

    fix: change

--- express-demo/routes/users.js
+++ express-demo/routes/users.js
@@ -51,6 +51,7 @@ router.get('/getVideoList', (req, res) => {
             console.log(chalk.red('err', err));
             return;
         }
+        console.log(rows);
         res.send(handler(rows));
     });
 });
